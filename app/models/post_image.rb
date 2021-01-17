230edaa5c68892518b6396b3f4efcbc5b606b983
masutaniR <popohu87@gmail.com>
2021-01-17 03:02:10 +0000

    [Add] 4-4 いいね機能

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,7 +2,7 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
-  has_many :favorites, dependent: :destory
+  has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
